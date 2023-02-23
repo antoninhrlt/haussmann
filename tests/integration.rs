ec6779ee6064b026c5709a18ca643347f23ce5cd
antoninhrlt <antonherault@gmail.com>
2023-02-23 16:10:07 +0100

    Update the tests following the changes

--- tests/integration.rs
+++ tests/integration.rs
@@ -2,34 +2,38 @@
 // Under the MIT License
 // Copyright (c) 2023 Antonin Hérault
 
-use haussmann::{Integrator, Direction};
+use haussmann::{Drawer, Direction};
 use haussmann::graphics::{Shape, calculate_size, Size, colours, Aligner};
 use haussmann::graphics::colours::RGBA;
 use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
-use sdl2::event::Event;
+use sdl2::event::{Event, WindowEvent};
 use sdl2::keyboard::Keycode;
 use sdl2::rect::Rect;
 use std::time::Duration;
 
 use haussmann::{widgets, Align, Widget, Overflow};
-use haussmann::widgets::{Button, Label, Layout, Image};
+use haussmann::widgets::{Button, Label, Layout, Image, Container};
 use haussmann::controllers::tap;
 
-struct DrawableZone {
-    pub canvas: sdl2::render::Canvas<sdl2::video::Window>,
+struct Canvas {
+    canvas: sdl2::render::Canvas<sdl2::video::Window>,
+    layout: Layout,
 }
 
-impl DrawableZone {
+impl Canvas {
+    /// Creates a canvas from a SDL2 window.
     fn from(window: sdl2::video::Window) -> Self {
         Self {
             canvas: window.into_canvas()
                 .present_vsync()
                 .build()
-                .unwrap()
+                .unwrap(),
+            layout: Layout::default(),
         }
     }
 
+    /// Changes the draw colour.
     fn change_colour(&mut self, colour: RGBA) {
         self.canvas.set_blend_mode(sdl2::render::BlendMode::Blend);
 
@@ -43,20 +47,20 @@ impl DrawableZone {
         );
     }
 
+    /// Clears the canvas to white.
     fn clear(&mut self) {
+        self.change_colour(RGBA::new(255, 255, 255, 255));
         self.canvas.clear();
     }
 
+    /// Swaps drawing buffer and canvas buffer in order to display the drawings.
     fn present(&mut self) {
         self.canvas.present();
     }
-
-    fn window_colour() -> RGBA {
-        RGBA::new(255, 255, 255, 255)
-    }
 }
 
-impl Integrator for DrawableZone {
+impl Drawer for Canvas {
+    /// Draws a `shape` on the canvas.
     fn shape(&mut self, shape: &Shape) { 
         let size: Size = calculate_size(shape);
 
@@ -80,12 +84,20 @@ impl Integrator for DrawableZone {
         }
     }
 
-    fn image(&mut self, image: &Image) {
-        //println!("renders image : {:?}", image);
+    fn image(&mut self, _image: &Image) {
+        todo!()
     }
 
-    fn label(&mut self, label: &Label) {
-        //println!("renders label : {:?}", label);
+    fn label(&mut self, _label: &Label) {
+        todo!()
+    }
+
+    fn set_layout(&mut self, layout: Layout) {
+        self.layout = layout;
+    }
+
+    fn layout(&self) -> &Layout {
+        &self.layout
     }
 }
 
@@ -93,99 +105,83 @@ impl Integrator for DrawableZone {
 fn with_sdl2() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    
+    // The current size of the window.
+    // Updated in the events loop.
+    let mut window_size: (i32, i32) = (400, 600);
 
-    let window = video_subsystem.window("Haussmann on SDL2", 400, 600)
+    let window = video_subsystem.window(
+        "Haussmann on SDL2", 
+        window_size.0 as u32, 
+        window_size.1 as u32
+    )
         .position_centered()
         .build()
         .unwrap();
 
-    let mut canvas = DrawableZone::from(window);
+    // Where to draw the widgets.
+    let mut canvas = Canvas::from(window);
 
-    let label = Label::simple("Hello!");
-
-    let button = tap::Detector::new(
-        Button::simple(
-            [100, 60], 
-            RGBA::new(255, 0, 0, 255),
-        ),
-        || {
-            println!("button1 was tapped!");
-        }
-    );
-
-    let button2 = Button::simple(
-        [80, 50], 
-        RGBA::new(0, 255, 0, 255),
-    );
-
-    let button3 = Button::simple(
-        [40, 40], 
-        RGBA::new(0, 0, 255, 255),
-    );
-
-    let child_layout = Layout::coloured(
-        [100, 100],
-        widgets![button3],
-        RGBA::new(0, 0, 0, 100),
-        Overflow::Hide,
+    // The layout organizing the widgets on the window.
+    let layout = Layout::simple(
+        widgets![
+            tap::Detector::new(
+                Button::simple(
+                    RGBA::new(255, 0, 0, 255),
+                ),
+                || {
+                    println!("button1 was tapped!");
+                }
+            ), 
+            Layout::coloured(
+                widgets![
+                    Button::simple(
+                        RGBA::new(0, 0, 255, 255),
+                    )
+                ],
+                RGBA::new(0, 0, 0, 50),
+                Overflow::Hide,
+                Align::Left,
+                Align::Bottom,
+                Direction::Column,
+            ), 
+            Button::simple(
+                RGBA::new(0, 255, 0, 255),
+            )
+        ],
+        Overflow::Ignore,
         Align::Center,
         Align::Center,
-        Direction::Column,
+        Direction::Row,
     );
 
-    let mut layout = Layout::fixed_coloured(
-        [50, 50],
-        [300, 500],
-        RGBA::new(0, 0, 0, 100),
-        widgets![label, button, child_layout, button2],
-        Overflow::Hide, 
-        Align::Center, 
-        Align::Center,
-        Direction::Column,
-    );
+    canvas.set_layout(layout);
 
     let mut event_pump = sdl_context.event_pump().unwrap();
-    
+
     'running: loop {
-        canvas.change_colour(DrawableZone::window_colour());
         canvas.clear();
 
-        // Draws the layout and its widgets.
-        canvas.layout(&mut layout);
-        
+        canvas.draw([25, 25], [window_size.0 as usize - 50, window_size.1 as usize - 50]);
+
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running
-                },
-                Event::MouseButtonDown {
-                    mouse_btn, 
-                    x, 
-                    y, 
-                    .. 
-                } => {
+                }
+                Event::Window { win_event, .. } => match win_event {
+                    WindowEvent::Resized(width, height) => {
+                        window_size = (width, height);
+                    }
+                    _ => {}
+                }
+                Event::MouseButtonDown {mouse_btn, x, y, .. } => {
                     if mouse_btn != MouseButton::Left {
                         return;
                     }
 
-                    let mut aligner = Aligner::new(&layout);
-
-                    for detector in layout.widgets::<tap::Detector>() {
-                        let mut widget: Shape = detector.shapes()[0].clone();
-                        aligner.align_shape(&mut widget);
-                        let position = widget.points()[0];
-                        let size = detector.tap_zone();
-                        let (x, y) = (x as isize, y as isize);
-
-                        if 
-                            (x >= position[0] && x <= position[0] + size[0] as isize) 
-                            && (y >= position[1] && y <= position[1] + size[1] as isize)
-                        {
-                            let callback: fn() = detector.on_tap;
-                            callback();
-                        }
-                    }
+                    todo!("on click on clickable widgets")
                 }
                 _ => {}
             }

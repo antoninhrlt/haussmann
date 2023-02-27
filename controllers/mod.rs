c2a50081cd22091f597543620e3c2581d64fe46b
antoninhrlt <antonherault@gmail.com>
2023-02-27 22:12:53 +0100

    General controller for all events

--- controllers/mod.rs
+++ controllers/mod.rs
@@ -2,17 +2,63 @@
 // Under the MIT License
 // Copyright (c) 2023 Antonin Hérault
 
-//! Controllers are wrappers for [`Widget`s](crate::Widget) having a function
-//! to call when a certain event happen.
-//!
-//! For example, to detect a widget has been tapped, there would be a wrapper
-//! like:
-//! ```
-//! TapDetector {
-//!     widget // Widget
-//!     on_tap // function
-//! }
-//! ```
-//! This controller actually exists and it is [`tap::Detector`].
+//! Controllers are wrappers for [`Widget`s](crate::Widget) having one or more 
+//! function/s to call when specific one or more event/s happen/s.
 
 pub mod tap;
+
+use crate::{Widget, widgets, ToAny, DebugWidget};
+
+/// Function to call when something happen on a widget.
+type ControllerFn<T> = fn(widget: &'static mut T);
+
+/// Wraps a widget to call functions when some events happen on it.
+#[derive(Debug)]
+pub struct Controller<T: Widget + 'static> {
+    /// The controlled widget.
+    pub widget: Box<T>,
+    /// Function to call when the widget is tapped. 
+    tap: Option<ControllerFn<T>>,
+    /// Function to call when the widget is focused.
+    focus: Option<ControllerFn<T>>,
+    /// Function to call when the widget is unfocused.
+    unfocus: Option<ControllerFn<T>>,
+}
+
+impl<T: Widget + 'static> Widget for Controller<T> {
+    fn shape(&self, position: Option<crate::graphics::Point>, size: Option<crate::graphics::Size>) -> crate::graphics::Shape {
+        self.widget.shape(position, size)
+    }
+}
+
+widgets::dynamic_controller!(Controller<T>);
+
+impl<T: Widget + 'static> Controller<T> {
+    /// Creates a new controller for a widget.
+    pub fn new(widget: T, tap: Option<ControllerFn<T>>, focus: Option<ControllerFn<T>>, unfocus: Option<ControllerFn<T>>) -> Self {
+        Self {
+            widget: Box::new(widget),
+            tap,
+            focus,
+            unfocus,
+        }
+    }
+
+    on!(tap, "tapped", on_tap); 
+    on!(focus, "focused", on_focus);
+    on!(unfocus, "unfocused", on_unfocus);
+}
+
+macro_rules! on {
+    ($field:ident, $x:expr, $function:ident) => {
+        #[allow(missing_docs)]
+        pub fn $function(&'static mut self) -> Result<(), String> {
+            match self.$field {
+                Some(function) => Ok(function(&mut self.widget)), 
+                None => Err(format!("undefined function when widget is {}", $x)),
+            }
+        }
+    };
+}
+
+pub(crate) use on;
--- controllers/tap.rs
+++ controllers/tap.rs
@@ -4,52 +4,39 @@
 
 //! Everything related to tapping widgets.
 
-use crate::{
-    graphics::{Shape, Size, Point},
-    DebugWidget, ToAny, Widget,
-};
+use crate::{Widget, widgets, ToAny, DebugWidget, graphics::{Point, Size, Shape}};
 
-// See here, however you need to handle lifetimes lol
-pub type OnTap<T> = fn(widget: &'static mut T);
+use super::ControllerFn;
 
-/// Controller wrapping a widget to detect when it is tapped.
+/// Controller detecting taps on a widget.
 #[derive(Debug)]
 pub struct Detector<T: Widget + 'static> {
-    /// Widget where the tap detection will be done.
+    /// The wrapped widget.
     pub widget: Box<T>,
-    /// Function called when the widget is tapped.
-    pub on_tap: OnTap<T>,
-    /// The zone covered by the detector, which is supposed to be the position 
-    /// and size of the widget.
-    pub zone: Option<(Point, Size)>,
+    /// Function to call when the widget is tapped.
+    tap: ControllerFn<T>,
 }
 
-crate::dynamic_controller!(Detector<T>);
+widgets::dynamic_controller!(Detector<T>);
 
-impl<T: Widget + 'static> Widget for Detector<T> {
-    /// Calls `Widget::shape()` on `self.widget` and returns the returned value
-    /// of this function.
+impl<T: Widget> Widget for Detector<T> {
     fn shape(&self, position: Option<Point>, size: Option<Size>) -> Shape {
         self.widget.shape(position, size)
     }
 }
 
 impl<T: Widget> Detector<T> {
-    /// Creates a new detector for a widget calling `on_tap` when it is tapped.
-    pub fn new(
-        widget: T,
-        on_tap: OnTap<T>,
-    ) -> Self {
+    /// Creates a new tap detector.
+    pub fn new(widget: T, tap: ControllerFn<T>) -> Self {
         Self {
             widget: Box::new(widget),
-            on_tap,
-            zone: None,
+            tap,
         }
     }
 
-    /// Function to call when the widget is tapped.
+    /// Function to call when the wrapped widget was tapped.
     pub fn on_tap(&'static mut self) {
-        let call = self.on_tap;
-        call(&mut self.widget);
+        let tap = self.tap;
+        tap(&mut self.widget);
     }
 }

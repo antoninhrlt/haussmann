7520eebb01adaa9186618b805268c76f91909865
antoninhrlt <antonherault@gmail.com>
2023-02-28 23:18:23 +0100

    Documentation for drawables and extract function

--- src/graphics/draw.rs
+++ src/graphics/draw.rs
@@ -6,11 +6,28 @@ use crate::{widgets::{Label, Image, Surface}, Widget};
 
 use super::{Size, Point};
 
+/// Something that can be drawn. Each element has a defined position and size 
+/// calculated at generation.
 #[derive(Debug)]
 pub enum Drawable {
+    /// Image to draw at a defined position with a defined size.
     Image(Image, Point, Size),
+    /// Label to draw at a defined position with a defined size.
     Label(Label, Point, Size),
+    /// Surface to draw at a defined position with a defined size.
     Surface(Surface, Point, Size),
-    
+    /// Unknown widget to draw at a defined position with a defined size.
     Unknown(Box<dyn Widget>, Point, Size),
+}
+
+impl Drawable {
+    /// Extracts the position and size of the drawable for the drawable itself.
+    pub fn extract(&self) -> (&Point, &Size) {
+        match self {
+            Self::Image(_, p, s) => (p, s),
+            Self::Label(_, p, s) => (p, s),
+            Self::Surface(_, p, s) => (p, s),
+            Self::Unknown(_, p, s) => (p, s),
+        }
+    }
 }

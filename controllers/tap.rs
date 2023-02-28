b625dd5858e99f2b108d6a00e96cd15b4175395d
antoninhrlt <antonherault@gmail.com>
2023-02-28 23:14:39 +0100

    Tap check function now out of the detector

--- controllers/tap.rs
+++ controllers/tap.rs
@@ -8,13 +8,27 @@ use crate::{Widget, widgets, ToAny, DebugWidget, graphics::{Point, Size, Shape, 
 
 use super::ControllerFn;
 
+/// Whether a tap is over the tappable zone.
+/// 
+/// This function must be called after the widget is drawn, never before.
+#[inline]
+pub fn is_tapped(tap: Point, widget_position: Point, widget_size: Size) -> bool {
+    let p: Point = widget_position;
+    let s: Size = widget_size;
+
+    let x = tap[0];
+    let y = tap[1];
+
+    x >= p[0] && x <= p[0] + s[0] as isize 
+        && y >= p[1] && y <= p[1] + s[1] as isize
+}
+
 /// Controller detecting taps on a widget.
 pub struct Detector<T: Widget> {
     /// The wrapped widget.
     pub widget: Box<T>,
-    /// The zone which is equal to the position and the size of the widget after
-    /// drawing.
-    pub zone: Option<(Point, Size)>,
+    /// The zone covered by the tap detection.
+    pub zone: (Point, Size),
     /// Function to call when the widget is tapped.
     tap: ControllerFn<T>,
 }
@@ -42,23 +56,9 @@ impl<T: Widget> Detector<T> {
     pub fn new(widget: T, tap: ControllerFn<T>) -> Self {
         Self {
             widget: Box::new(widget),
-            zone: None,
+            zone: ([-1, -1], [0, 0]),
             tap,
         }
-    }
-
-    /// Whether a tap is over the tappable zone.
-    /// 
-    /// This function must be called after the widget is drawn, never before.
-    pub fn is_tapped(&self, tap: Point) -> bool {
-        let p: Point = self.zone.unwrap().0;
-        let s: Size = self.zone.unwrap().1;
-
-        let x = tap[0];
-        let y = tap[1];
-
-        x >= p[0] && x <= p[0] + s[0] as isize 
-            && y >= p[1] && y <= p[1] + s[1] as isize
     }
 
     /// Function to call when the wrapped widget was tapped.

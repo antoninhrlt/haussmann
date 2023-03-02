7d3212491aa5cf0a16b4ad6e82bdb3a4da462d3b
antoninhrlt <antonherault@gmail.com>
2023-03-02 23:31:15 +0100

    Test for unusual usages of the project

---
+++ tests/unusual.rs
@@ -0,0 +1,39 @@
+//! Any usage of haussmann that might be unusual and that might go into panics
+
+use haussmann::{view, layout, Align, widgets::{Layout, View, Surface, Widget}, widgets, Overflow, Direction, rgba, graphics::colours::RGBA};
+
+#[test]
+fn no_widget() {
+    let view = View::new(
+        [0, 0],
+        [1, 1],
+        Layout::simple(
+            Overflow::Ignore,
+            Align::Center,
+            Align::Center,
+            Direction::Column,
+            widgets![],
+        ),
+    );
+
+    view.build();
+}
+
+#[test]
+fn void_view() {
+    let view = View::new(
+        [0, 0],
+        [0, 0],
+        Layout::simple(
+            Overflow::Ignore,
+            Align::Center,
+            Align::Center,
+            Direction::Column,
+            widgets![
+                Surface::coloured(rgba!(255, 255, 255, a: 255))
+            ],
+        ),
+    );
+
+    view.build();
+}

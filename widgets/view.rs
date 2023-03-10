823106884d9c0c6b9ff770763842d382ceaa8477
antoninhrlt <antonherault@gmail.com>
2023-03-10 22:22:48 +0100

    Update controllers zones with the right drawable

--- widgets/view.rs
+++ widgets/view.rs
@@ -4,7 +4,7 @@
 
 use crate::controllers::Controller;
 use crate::graphics::{Point, Size};
-use crate::graphics::draw::{Drawable, self};
+use crate::graphics::draw::{Drawable, DrawableAt, self};
 use crate::Zone;
 
 use super::Layout;
@@ -61,19 +61,25 @@ impl View {
         self.build()
     }
 
+
+    /// Browses all the widgets to find controllers and call the given callback
+    /// when encountered.
+    /// 
+    /// Browses in the same order as drawables are created.
+    pub fn controllers<T>(&mut self, drawables: &Vec<Drawable>, callback: impl Fn(&mut T)) 
+        where T: Controller + 'static,
+    {
+        let mut i: usize = 1;
+        Self::controllers_in(&mut self.layout, drawables, &callback, &mut i);
+    }
+}
+
+impl View {
+    /// Checks [`controllers()`].
     fn controllers_in<T>(layout: &mut Layout, drawables: &Vec<Drawable>, callback: &impl Fn(&mut T), i: &mut usize) 
         where T: Controller + 'static,
     {
         for widget in &mut layout.widgets {
-            for j in *i..drawables.len() - 2 {
-                if drawables[j].parent_id == drawables[j + 1].parent_id {
-                    *i += 1;
-                    continue;
-                }
-
-                break
-            }
-
             // Encounters a layout, calls itself but with the retrieved layout 
             // as parameter.
             if let Some(layout) = widget.as_any_mut().downcast_mut::<Layout>() {
@@ -85,21 +91,12 @@ impl View {
             // Encounters a controller, calls the callback.
             if let Some(controller) = widget.as_any_mut().downcast_mut::<T>() {
                 // Update the controller's zone.
-                controller.update(drawables[*i].zone);
+                controller.update(drawables.at(*i).unwrap().zone);
                 // Calls the callback giving the controller as mutable reference.
                 callback(controller);
             }
-        }
-    }
 
-    /// Browses all the widgets to find controllers and call the given callback
-    /// when encountered.
-    /// 
-    /// Browses in the same order as drawables are created.
-    pub fn controllers<T>(&mut self, drawables: &Vec<Drawable>, callback: impl Fn(&mut T)) 
-        where T: Controller + 'static,
-    {
-        let mut i: usize = 1;
-        Self::controllers_in(&mut self.layout, drawables, &callback, &mut i);
+            *i += 1;
+        }
     }
 }
